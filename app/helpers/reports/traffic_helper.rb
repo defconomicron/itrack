0e452550a3cd6c6878a4056f426dc2372659d2ca
Administrator <administrator@administrator-laptop.(none)>
2009-01-09 00:21:44 -0600

    graph

--- app/helpers/reports/traffic_helper.rb
+++ app/helpers/reports/traffic_helper.rb
@@ -1,6 +1,6 @@
 module Reports::TrafficHelper
   def format_datetime1(s)
-    return s.to_time.strftime("%a %m-%d-%Y %I:%M %p") if s.to_time.min == 1
+    return s.to_time.strftime("%a %m-%d-%Y %I:%M %p") if s.to_time.min == 0
     s.to_time.strftime("%I:%M %p")
   end
 
