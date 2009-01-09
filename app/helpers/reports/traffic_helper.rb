f1fd94a7404aaff50449e2518ecb6bd04c4afb49
Administrator <administrator@administrator-laptop.(none)>
2009-01-09 00:28:16 -0600

    time range

--- app/helpers/reports/traffic_helper.rb
+++ app/helpers/reports/traffic_helper.rb
@@ -8,4 +8,8 @@ module Reports::TrafficHelper
     return s.to_time.strftime("%a %m-%d-%Y %I %p") if s.to_time.hour == 0
     s.to_time.strftime("%I %p")
   end
+  
+  def format_datetime3(s)
+    s.to_time.strftime("%a %m-%d-%Y %I:%M:%S %p")
+  end
 end
