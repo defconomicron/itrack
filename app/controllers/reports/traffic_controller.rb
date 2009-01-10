9b22808eec9a51570a3cc8309b50d943e68fb4f6
Administrator <administrator@administrator-laptop.(none)>
2009-01-09 18:51:53 -0600

    summary

--- app/controllers/reports/traffic_controller.rb
+++ app/controllers/reports/traffic_controller.rb
@@ -1,6 +1,14 @@
 class Reports::TrafficController < Reports::BaseController
   def index
     @hits = Hit.traffic(params)
+    @total_hits = 0
+    @total_visits = 0
+    @total_visitors = 0
+    @hits.each do |h|
+      @total_hits += h["hits"].to_i
+      @total_visits += h["visits"].to_i
+      @total_visitors += h["visitors"].to_i
+    end
   end
   
   def search
