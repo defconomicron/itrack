8a4ba6f1b02c043ab24e91b738e8220214e59ff1
Administrator <administrator@administrator-desktop.(none)>
2009-01-13 23:40:49 -0600

    graph datetime format

--- app/helpers/reports/traffic_helper.rb
+++ app/helpers/reports/traffic_helper.rb
@@ -1,12 +1,19 @@
 module Reports::TrafficHelper
-  def format_datetime1(s)
-    return s.to_time.strftime("%a %m-%d-%Y %I:%M %p") if s.to_time.min == 0
-    s.to_time.strftime("%I:%M %p")
+  def boolToNum(bVal)
+    bVal ? 1 : 0
   end
-
-  def format_datetime2(s)
-    return s.to_time.strftime("%a %m-%d-%Y %I %p") if s.to_time.hour == 0
-    s.to_time.strftime("%I %p")
+  
+  def renderChartHTML(chartSWF, strURL, strXML, chartId, chartWidth, chartHeight, debugMode)
+    strFlashVars = "&chartWidth=#{chartWidth}&chartHeight=#{chartHeight}&debugMode=#{boolToNum(debugMode)}" << (strXML == "" ? "&dataURL=#{strURL}" : "&dataXML=#{strXML}")
+		"<embed wmode=\"transparent\" src=\"#{chartSWF}\" FlashVars=\"#{strFlashVars}\" quality=\"high\" width=\"#{chartWidth}\" height=\"#{chartHeight}\" name=\"#{chartId}\" allowScriptAccess=\"always\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\" />"    
+  end
+  
+  def format_datetime(s, prev)
+    return s.to_time.strftime("%a %I:%M %p") if params[:period] == "minute" && prev && s.to_time.hour != prev.to_time.hour
+    return s.to_time.strftime("%I:%M %p")    if params[:period] == "minute" && prev && s.to_time.hour == prev.to_time.hour
+    
+    return s.to_time.strftime("%a %I %p") if params[:period] == "hour" && prev && s.to_time.day != prev.to_time.day
+    return s.to_time.strftime("%I %p")    if params[:period] == "hour" && prev && s.to_time.day == prev.to_time.day
   end
   
   def format_datetime3(s)
