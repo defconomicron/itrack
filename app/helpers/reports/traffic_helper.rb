8943cfff944cce403e4a83670477a6ae76d687c3
Adam Hernandez <adam@digimedia.com>
2009-01-14 08:49:21 -0600

    datetime on graph fixed

--- app/helpers/reports/traffic_helper.rb
+++ app/helpers/reports/traffic_helper.rb
@@ -5,15 +5,19 @@ module Reports::TrafficHelper
   
   def renderChartHTML(chartSWF, strURL, strXML, chartId, chartWidth, chartHeight, debugMode)
     strFlashVars = "&chartWidth=#{chartWidth}&chartHeight=#{chartHeight}&debugMode=#{boolToNum(debugMode)}" << (strXML == "" ? "&dataURL=#{strURL}" : "&dataXML=#{strXML}")
-		"<embed wmode=\"transparent\" src=\"#{chartSWF}\" FlashVars=\"#{strFlashVars}\" quality=\"high\" width=\"#{chartWidth}\" height=\"#{chartHeight}\" name=\"#{chartId}\" allowScriptAccess=\"always\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\" />"    
+    "<embed wmode=\"transparent\" src=\"#{chartSWF}\" FlashVars=\"#{strFlashVars}\" quality=\"high\" width=\"#{chartWidth}\" height=\"#{chartHeight}\" name=\"#{chartId}\" allowScriptAccess=\"always\" type=\"application/x-shockwave-flash\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\" />"    
   end
   
   def format_graph_datetime(curr, prev)
-    return curr.to_time.strftime("%a %I:%M %p") if params[:period] == "minute" && (prev.nil? || curr.to_time.hour != prev.to_time.hour)
-    return curr.to_time.strftime("%I:%M %p")    if params[:period] == "minute" && (prev.nil? || curr.to_time.hour == prev.to_time.hour)
+    if (prev.nil? || curr.to_time.day != prev.to_time.day)
+      return curr.to_time.strftime("%a %I:%M %p") if params[:period] == "minute"
+      return curr.to_time.strftime("%a %I %p") if params[:period] == "hour"
+    end
     
-    return curr.to_time.strftime("%a %I %p") if params[:period] == "hour" && (prev.nil? || curr.to_time.day != prev.to_time.day)
-    return curr.to_time.strftime("%I %p")    if params[:period] == "hour" && (prev.nil? || curr.to_time.day == prev.to_time.day)
+    if (prev.nil? || curr.to_time.day == prev.to_time.day)
+      return curr.to_time.strftime("%I:%M %p") if params[:period] == "minute"
+      return curr.to_time.strftime("%I %p") if params[:period] == "hour"
+    end
   end
   
   def format_datetime(s)
